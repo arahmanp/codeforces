b3e9f37c934559fa1db06f5d56ca1551a8ecd473
Andhika Rahman <andhikarhmnp@gmail.com>
2026-06-30 11:19:41 +0700

    CODEFORCES | 4A - Watermelon (Testing)

---
+++ 4a_watermelon.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Tulis solusi lo di sini
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    // cin >> t; // uncomment kalau ada multi-testcase
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
